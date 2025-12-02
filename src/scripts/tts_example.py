1b3c906897fc7a049c862e506675c40ca5e7a106
Dan Saattrup Nielsen <saattrupdan@gmail.com>
2025-12-02 12:40:55 +0100

    feat: Add tts-example script

---
+++ src/scripts/tts_example.py
@@ -0,0 +1,37 @@
+"""Run a TTS example."""
+
+from pathlib import Path
+
+import torchaudio as ta
+from chatterbox.mtl_tts import ChatterboxMultilingualTTS
+from huggingface_hub import hf_hub_download
+
+REPO_ID = "CoRal-project/tts-base-compatible"
+
+
+def main() -> None:
+    """Run the TTS example."""
+    local_path = ""
+    for fpath in [
+        "ve.pt",
+        "t3_mtl23ls_v2.safetensors",
+        "s3gen.pt",
+        "grapheme_mtl_merged_expanded_v1.json",
+        "conds.pt",
+    ]:
+        local_path = hf_hub_download(repo_id=REPO_ID, filename=fpath)
+
+    # Load the model
+    checkpoint_dir = Path(local_path).parent  # Same for all files
+    model = ChatterboxMultilingualTTS.from_local(checkpoint_dir, device="mps")
+
+    # Generate speech to a wav file
+    text = "Dette er en test!"
+    wav = model.generate(
+        text, language_id="da", exaggeration=1.0, cfg_weight=0.5, temperature=0.4
+    )
+    ta.save(uri="test.wav", src=wav, sample_rate=model.sr)
+
+
+if __name__ == "__main__":
+    main()
